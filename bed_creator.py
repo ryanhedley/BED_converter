31522c341f083622fa0e07f4739d90f820d4cdf6
Ryan Hedley <ryan.hedley+git@gmail.com>
2020-10-03 13:31:36 +0800

    Tidied code for posting

--- bed_creator.py
+++ bed_creator.py
@@ -2,54 +2,27 @@ import pandas as pd
 from tkinter.filedialog import askopenfilename
 
 
-#TODO: FTP import BED file
+#Import BED file
 def openfile():
     bed_filename = askopenfilename(title = "Select file", filetypes = (("BED files","*.bed"),("All Files","*.*")))
     return (bed_filename)
 
 
 
-#TODO: Convert BED file to DF
+#Convert BED file to DF
 def convert_bed_to_df(bed_filename):
     #columns = ['chr', 'start', 'stop', 'gene', 'dosage']
     bed_df = pd.read_csv(bed_filename, sep="\t", header=1 )
     print("Reading... ",bed_filename)
 
-
-
-
-    # for index, row in bed_df.iterrows():
-    #     # print(row[1])
-    #     # chromosome = str(bed_df[0][ind])
-    #     dosage_score = int(row[4])
-    #     print(dosage_score)
-    #     if dosage_score == 0:
-    #         dosage_colour = score_0
-    #     if dosage_score == 1:
-    #         dosage_colour = score_1
-    #     if dosage_score == 2:
-    #         dosage_colour = score_2
-    #     if dosage_score == 3:
-    #         dosage_colour = score_3
-    #     if dosage_score == 30:
-    #         dosage_colour = score_30
-    #     if dosage_score == 40:
-    #         dosage_colour = score_40
-    #     else:
-    #         dosage_colour = score_0
-    
-    #     bed_df.loc[index, 5] == dosage_colour
-    #     print(dosage_colour)
-
-
-
     return(bed_df)
 
 
-#TODO: Convert BED to required format
+#Convert BED to required format
 def convert_df_to_bed(bed_filename, bed_df):
     bed_filename = bed_filename + '_converted.bed'
 
+    # RGB Colours
     score_0 = "0,255,0"     #Green
     score_1 = "255,255,0"   #Yellow
     score_2 = "255,125,0"   #Orange
@@ -64,6 +37,7 @@ def convert_df_to_bed(bed_filename, bed_df):
         for index, row in bed_df.iterrows():
             dosage_score = int(row[4])
 
+            # Determine the colour from the dosage score
             if dosage_score == 0:
                 dosage_colour = score_0
             if dosage_score == 1:
@@ -77,10 +51,8 @@ def convert_df_to_bed(bed_filename, bed_df):
             if dosage_score == 40:
                 dosage_colour = score_40
 
-            print(dosage_score,dosage_colour)
-
             
-            
+            # Create each line to add to new BED file
             line = (
                 str(row[0]) + "\t" +        #chr
                 str(row[1]) + "\t" +        #start
@@ -95,9 +67,7 @@ def convert_df_to_bed(bed_filename, bed_df):
 
             f.writelines(line)
 
-    # bed_df.to_csv (bed_filename, index=False, sep='\t', mode='a')
     print("Writing... ",bed_filename)
-
 
 
 
@@ -105,7 +75,6 @@ def main():
     bed_filename = openfile()
     bed_df = convert_bed_to_df(bed_filename)
     convert_df_to_bed(bed_filename, bed_df)
-
 
 
 
